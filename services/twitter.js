a3afb197698517f13feac07b23f391f6ec97cd78
Sinisa Vujic <sv.svujic@gmail.com>
2014-09-23 02:40:41 +0200

    better handling of env('TWITTER_PUBLISHING')

--- services/twitter.js
+++ services/twitter.js
@@ -3,13 +3,21 @@
 var Twit = require('twit');
 var winston = require('winston');
 var env = require('../lib/env');
-var client = new Twit({
-  consumer_key:        env('TWITTER_CONSUMER_KEY'),
-  consumer_secret:     env('TWITTER_CONSUMER_SECRET'),
-  access_token:        env('TWITTER_ACCESS_TOKEN_KEY'),
-  access_token_secret: env('TWITTER_ACCESS_TOKEN_SECRET'),
-});
-var enabled = env('TWITTER_PUBLISHING');
+var client;
+var enabled = env('TWITTER_PUBLISHING') && (function (a) {
+  try {
+    a = new Twit({
+      consumer_key:        env('TWITTER_CONSUMER_KEY'),
+      consumer_secret:     env('TWITTER_CONSUMER_SECRET'),
+      access_token:        env('TWITTER_ACCESS_TOKEN_KEY'),
+      access_token_secret: env('TWITTER_ACCESS_TOKEN_SECRET'),
+    });
+    return true;
+  } catch (e) {
+    winston.error('new Twit failed ', e.stack);
+    return false;
+  }
+})(client);
 
 function noop () {}
 
